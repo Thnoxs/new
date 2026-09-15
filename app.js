67cb73044886a1f0547e50fa400a07ccf514ecff
Thnoxs <thnoxs@gmail.com>
2026-09-15 06:15:42 +0530

    Added: I have massed some more logic

---
+++ app.js
@@ -0,0 +1,50 @@
+const boxs = document.querySelectorAll(".box");
+const win = new Audio("asstes/arcade-game-jump.wav");
+const loose = new Audio("asstes/wrong-answer.wav");
+const img = document.querySelectorAll(".img-box");
+const lifeSpan = document.querySelectorAll(".lifeSpan");
+const lifeUpdater = document.getElementById("lifeUpdater");
+const boomConatiner = [];
+let totalLife = 3;
+for (let i = 0; i < boxs.length - 27; i++) {
+  let numberString = Math.floor(1 + Math.random() * boxs.length).toString();
+  boomConatiner.push(numberString);
+}
+console.log(boomConatiner);
+boxs.forEach((el) => {
+  el.addEventListener("click", function () {
+    const index = el.getAttribute("data-index");
+    if (boomConatiner.includes(index)) {
+      totalLife--;
+      lifeUpdater.textContent = totalLife;
+      if (totalLife > 0) {
+        el.classList.add("bom");
+        clear(boxs, "selected");
+        img[Number(index - 1)].src = "/asstes/tnt.png";
+        loose.play();
+      } else {
+        img[Number(index - 1)].src = "/asstes/tnt.png";
+        clear(boxs, "selected");
+        el.classList.add("bom");
+        loose.play();
+        setTimeout(() => {
+          location.reload();
+          totalLife = 3;
+        }, 3000);
+      }
+    } else {
+      win.play();
+      img[Number(index - 1)].src = "/asstes/basket.png";
+      el.classList.add("selected");
+    }
+  });
+});
+
+function clear(el, className) {
+  return el.forEach((e) => {
+    setTimeout(() => {
+      e.classList.remove(className);
+      e.classList.remove("bom");
+    }, 1000);
+  });
+}
